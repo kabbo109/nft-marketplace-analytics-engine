fdd72c763d4d6f202f232712f01e2f606abbdd9a
Kabbo | Frontend Developer <160781484+kabbo109@users.noreply.github.com>
2026-01-18 14:05:01 +0600

    Create nft.registry.js

---
+++ nft.registry.js
@@ -0,0 +1,16 @@
+export function loadNFTs() {
+  console.log("Loading NFT registry...");
+
+  return [
+    {
+      name: "Genesis Ape",
+      collection: "Ape Collection",
+      floorPrice: 1.1
+    },
+    {
+      name: "Cyber Cat",
+      collection: "Cyber Pets",
+      floorPrice: 0.6
+    }
+  ];
+}
